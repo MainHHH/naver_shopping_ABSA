0930e48f0077b22f26f0c6d21d690b808d11d2f5
MainHHH <bmn0330@gmail.com>
2024-12-03 14:32:45 +0900

    [feat] crawling test

--- smartstore_reviews/smartstore_reviews/settings.py
+++ smartstore_reviews/smartstore_reviews/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "smartstore_reviews.spiders"
 #USER_AGENT = "smartstore_reviews (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
