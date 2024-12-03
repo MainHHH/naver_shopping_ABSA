27ad742bafa0e947e1f9a4d1e1315b3ddbdfa39b
MainHHH <bmn0330@gmail.com>
2024-12-03 13:31:55 +0900

    [feat] gen spider

---
+++ smartstore_reviews/smartstore_reviews/spiders/reviews.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class ReviewsSpider(scrapy.Spider):
+    name = "reviews"
+    allowed_domains = ["smartstore.naver.com"]
+    start_urls = ["https://smartstore.naver.com/i/v1/contents/reviews/query-pages"]
+
+    def parse(self, response):
+        pass
