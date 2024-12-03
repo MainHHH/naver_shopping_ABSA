5f13b324c8f3464e000e731b4192b015a8bbcd06
MainHHH <bmn0330@gmail.com>
2024-12-03 14:32:23 +0900

    [feat] crawling test

--- smartstore_reviews/smartstore_reviews/spiders/reviews.py
+++ smartstore_reviews/smartstore_reviews/spiders/reviews.py
@@ -4,8 +4,27 @@ import scrapy
 class ReviewsSpider(scrapy.Spider):
     name = "reviews"
     allowed_domains = ["smartstore.naver.com"]
-    start_urls = ["https://smartstore.naver.com/i/v1/contents/reviews/query-pages"]
+
+    def start_requests(self):
+        start_urls = "https://smartstore.naver.com/i/v1/contents/reviews/query-pages"
+        formdata = {
+            "checkoutMerchantNo": "500235951",
+            "originProductNo": "4729223012",
+            "page": "1",
+            "pageSize": "20",
+            "reviewSearchSortType": "REVIEW_RANKING"
+        }
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+        }
+
+        yield scrapy.FormRequest(
+            url=start_urls,
+            callback=self.parse,
+            formdata=formdata,
+            method='POST',
+            headers=headers,
+        )
 
     def parse(self, response):
-
-        pass
+        return response
