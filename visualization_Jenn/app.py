63f690de93402af6b0f11b848c919b8cdbd9f195
heeaejenn <heeaejenn@gmail.com>
2024-12-18 19:00:22 +0900

    [feat] create navigator and pages

--- visualization_Jenn/app.py
+++ visualization_Jenn/app.py
@@ -6,17 +6,16 @@ st.set_page_config(page_title="네이버 리뷰 ABSA 분석기", layout="centere
 # 홈 목록
 start = st.Page("pages/start.py", title="시작하기", default=True)
 
-# 영화 목록
-test = st.Page("pages/test.py", title='summary')
+# 분석 도구 목록
+status = st.Page("pages/status.py", title='현황 확인')
+analysis = st.Page('pages/analysis.py', title='리뷰 심층 분석')
 
 pg = st.navigation(
     {
         "홈": [start],
-        "분석 도구": [test],
+        "분석 도구": [status, analysis],
     }
 )
 pg.run()
-
-# st.sidebar.success("리뷰 분석 도구를 선택해주세요.")
 
 
